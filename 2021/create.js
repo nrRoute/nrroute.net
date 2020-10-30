01c14a87a3f85c3956199232073930aa30759e49
nrRoute <49706844+nrRoute@users.noreply.github.com>
2020-10-31 00:09:52 +0900

    Add files via upload

---
+++ 2021/create.js
@@ -0,0 +1,6 @@
+var check = function() {
+    var get = document.getElementById('plain').value;
+    var ret = ""
+    if(get == "あああ") ret = "aaa";
+    document.getElementById('brain').value = ret;
+}
