494edeb15e787b664e3d582c851290442df1ee16
nrRoute <49706844+nrRoute@users.noreply.github.com>
2022-05-31 14:51:14 +0900

    Delete create.js

--- 2021/create.js
+++
@@ -1,8 +0,0 @@
-var check = function() {
-    var get = document.getElementById('plain').value;
-    var ret = ""
-    if(get == "あああ") ret = "aaa";
-    else if(get == "newyear") ret = "Happy New Year!";
-    else ret = "Invalid";
-    document.getElementById('brain').value = ret;
-}
