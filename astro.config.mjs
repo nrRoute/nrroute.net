5dec11a89395638e3797437755190a0774441685
nrRoute <kaleidoscope9784@gmail.com>
2024-10-08 17:18:54 +0900

    feat: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nrroute.github.io',
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap()]
 });
