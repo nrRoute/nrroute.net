fb1d1c71a627f879547901e8e2b1215c2a147774
nrRoute <kaleidoscope9784@gmail.com>
2024-12-12 16:09:23 +0900

    update: use Content Collection

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import tailwind from "@astrojs/tailwind";
 
 import sitemap from '@astrojs/sitemap';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nrroute.github.io',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), mdx()]
 });
