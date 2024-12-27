76f60793addea24d764f7488e776feca2f56a83d
nrRoute <kaleidoscope9784@gmail.com>
2024-12-28 08:21:40 +0900

    add: @playform/compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import sitemap from '@astrojs/sitemap';
 
 import mdx from '@astrojs/mdx';
 
+import playformCompress from '@playform/compress';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nrroute.github.io',
-  integrations: [sitemap(), mdx()]
+  integrations: [sitemap(), mdx(), playformCompress()]
 });
