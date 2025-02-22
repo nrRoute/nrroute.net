b38788a70612c82819cd805a0a5c05fa373a1643
nrRoute <kaleidoscope9784@gmail.com>
2025-02-23 06:26:53 +0900

    add: expressive code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,31 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
-
 import mdx from '@astrojs/mdx';
 
 import playformInline from '@playform/inline';
-
 import purgecss from 'astro-purgecss';
-
 import rlc from 'remark-link-card';
-
 import remarkGemoji from 'remark-gemoji';
+import astroExpressiveCode from 'astro-expressive-code';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nrroute.github.io',
   integrations: [
     sitemap(),
+    astroExpressiveCode({
+      themes: ["github-dark-high-contrast", "github-light-high-contrast"],
+      styleOverrides: {
+        frames: {
+          frameBoxShadowCssValue: "none",
+        }
+      }
+    }),
     mdx(),
     playformInline(),
     purgecss({
       fontFace: true,
-    })
+    }),
   ],
   build: {
     format: "file",
