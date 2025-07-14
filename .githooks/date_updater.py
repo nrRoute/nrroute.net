185c0bf8359c4ca8037421c1ee98f5c0f6cc1500
nrRoute <kaleidoscope9784@gmail.com>
2025-07-15 02:17:07 +0900

    update: pre-commit script

--- .githooks/date_updater.py
+++ .githooks/date_updater.py
@@ -4,7 +4,7 @@ from pathlib import Path
 for name in sys.argv[1:]:
     path = Path(name)
     post = frontmatter.load(path)
-    if post.get("created"):
+    if post.get("created") < datetime.datetime.today().date():
         post["modified"] = datetime.datetime.today().date()
         frontmatter.dump(post, path, sort_keys=False)
         subprocess.run(["git", "add", str(path)], check=True)
