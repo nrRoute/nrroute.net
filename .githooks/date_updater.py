2a8945441f67a064d103b84900927944c0db6b2f
nrRoute <kaleidoscope9784@gmail.com>
2025-07-14 16:11:20 +0900

    add: pre-commit content date updater

---
+++ .githooks/date_updater.py
@@ -0,0 +1,10 @@
+import sys, frontmatter, datetime, subprocess
+from pathlib import Path
+
+for name in sys.argv[1:]:
+    path = Path(name)
+    post = frontmatter.load(path)
+    if post.get("created"):
+        post["modified"] = datetime.datetime.today().date()
+        frontmatter.dump(post, path, sort_keys=False)
+        subprocess.run(["git", "add", str(path)], check=True)
