378dd1faed2799e4c36229d83d59a3e227c02b90
nrRoute <kaleidoscope9784@gmail.com>
2025-07-16 09:26:45 +0900

    update: add eof

--- .githooks/date_updater.py
+++ .githooks/date_updater.py
@@ -4,7 +4,11 @@ from pathlib import Path
 for name in sys.argv[1:]:
     path = Path(name)
     post = frontmatter.load(path)
+
     if post.get("created") < datetime.datetime.today().date():
         post["modified"] = datetime.datetime.today().date()
-        frontmatter.dump(post, path, sort_keys=False)
-        subprocess.run(["git", "add", str(path)], check=True)
+
+    text = frontmatter.dumps(post, sort_keys=False).rstrip("\n") + "\n"
+    path.write_text(text, encoding="utf-8")
+
+    subprocess.run(["git", "add", str(path)], check=True)
