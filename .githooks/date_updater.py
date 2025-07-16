49b52619a0f5c1ec8efab3581895f1425f77c1d5
nrRoute <kaleidoscope9784@gmail.com>
2025-07-17 05:16:22 +0900

    fix: pre-commit hooks

--- .githooks/date_updater.py
+++ .githooks/date_updater.py
@@ -6,9 +6,7 @@ for name in sys.argv[1:]:
     post = frontmatter.load(path)
 
     if post.get("created") < datetime.datetime.today().date():
-        post["modified"] = datetime.datetime.today().date()
-
-    text = frontmatter.dumps(post, sort_keys=False).rstrip("\n") + "\n"
-    path.write_text(text, encoding="utf-8")
-
-    subprocess.run(["git", "add", str(path)], check=True)
+        if post.get("modified") and post.get("modified") < datetime.datetime.today().date():
+            post["modified"] = datetime.datetime.today().date()
+            text = frontmatter.dumps(post, sort_keys=False).rstrip("\n") + "\n"
+            path.write_text(text, encoding="utf-8")
