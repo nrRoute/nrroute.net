a0b6bc0107a2b09b051a932e449985dec1c62448
nrRoute <kaleidoscope9784@gmail.com>
2025-07-17 05:22:02 +0900

    fix: condition for update

--- .githooks/date_updater.py
+++ .githooks/date_updater.py
@@ -6,7 +6,7 @@ for name in sys.argv[1:]:
     post = frontmatter.load(path)
 
     if post.get("created") < datetime.datetime.today().date():
-        if post.get("modified") and post.get("modified") < datetime.datetime.today().date():
+        if not post.get("modified") or post.get("modified") < datetime.datetime.today().date():
             post["modified"] = datetime.datetime.today().date()
             text = frontmatter.dumps(post, sort_keys=False).rstrip("\n") + "\n"
             path.write_text(text, encoding="utf-8")
